59c26527a57b26963ec2ad6eba87f154d4aaf642
FedericoBellezza <federico.bellezza2001@gmail.com>
2025-02-12 14:49:21 +0100

    add daily in details

--- src/pages/DayWeatherPage.jsx
+++ src/pages/DayWeatherPage.jsx
@@ -1,24 +1,19 @@
 import { useParams } from "react-router";
 import { useGlobalContext } from "../context/GlobalContext";
-import { useEffect } from "react";
 
 export default function DayWeatherPage() {
-  const { searchedCity, setWeatherResult } = useGlobalContext();
+  const { id } = useParams();
+  const { searchedCity, setWeatherResult, weatherResult } = useGlobalContext();
+  console.log(id);
 
-  useEffect(() => {
-    // function to get wheather from coordinates
-    const getWheatherHourly = (city) => {
-      fetch(
-        `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=precipitation_sum&daily=weather_code,temperature_2m_max,temperature_2m_min,precipitation_probability_max&timezone=auto&past_days=1`
-      )
-        .then((res) => res.json())
-        .then((data) => {
-          setWeatherResult(data);
-          console.log(data);
-        })
-        .catch((err) => console.error(err));
-    };
-  }, []);
+  // on page reload
+  !searchedCity && window.location.replace("/");
+
+  console.log(weatherResult.daily.precipitation_probability_max);
+
+  const getJustTime = (e) => {
+    return e.slice(-5);
+  };
 
   return (
     <>
@@ -27,15 +22,85 @@ export default function DayWeatherPage() {
       <i className="fa-solid fa-sun text-[30rem] xl:text-[60rem] text-white fixed opacity-35 xl:top-30 xl:left-[-10rem] top-100 left-[-15rem]  z-[-1]"></i>
 
       <div className="mx-auto my-0 max-w-screen overflow-hidden bg-linear-to-tr  min-h-screen h-full flex flex-col items-center pb-50 ">
-        <h1 className="xl:text-7xl text-4xl font-black text-white text-center py-15">
-          {searchedCity}
+        <h1 className="xl:text-5xl lg:text-7xl text-4xl font-black text-white text-center lg:mt-10 py-10">
+          {searchedCity ? searchedCity.address.city : "Città non impostata"}
         </h1>
 
-        <div className="p-10 ">
-          <h1 className="text-3xl mb-5">DayWeatherPage</h1>
-          <ul>
+        <div className=" text-white text-center">
+          <h1 className="text-4xl lg:text-5xl font-thin mb-15">Daily</h1>
+
+          <div className="flex flex-wrap flex-row  gap-3 lg:gap-5  justify-between items-center lg:px-50 px-10 w-screen">
+            {/* Precipitation probability */}
+            <div className="flex flex-col items-center justify-between  md:w-50 w-35    gap-3 bg-slate-500 rounded-2xl p-5  ">
+              <i className="fa-solid fa-cloud-rain text-5xl lg:text-7xl"></i>
+              <div className="md:text-2xl text-lg ">
+                Probabilità <br />
+                {weatherResult.daily.precipitation_probability_max[id]} %
+              </div>
+            </div>
+            {/* Precipitation probability */}
+            <div className="flex flex-col items-center justify-between  md:w-50 w-35    gap-3 bg-slate-500 rounded-2xl p-5  ">
+              <i className="fa-solid fa-umbrella text-5xl lg:text-7xl"></i>
+              <div className="md:text-2xl text-lg ">
+                Quantità
+                <br /> {weatherResult.daily.precipitation_sum[id]} mm
+              </div>
+            </div>
+            {/* Precipitation probability */}
+            <div className="flex flex-col items-center justify-between md:w-50 w-35   gap-3 bg-slate-500 rounded-2xl p-5  ">
+              <i className="fa-solid fa-sun text-5xl lg:text-7xl"></i>
+              <div className="md:text-2xl text-lg ">
+                Alba <br /> {getJustTime(weatherResult.daily.sunrise[id])}
+              </div>
+            </div>
+            {/* Precipitation probability */}
+            <div className="flex flex-col items-center justify-between md:w-50 w-35   gap-3 bg-slate-500 rounded-2xl p-5  ">
+              <i className="fa-solid fa-moon text-5xl lg:text-7xl"></i>
+              <div className="md:text-2xl text-lg ">
+                Tramonto <br /> {getJustTime(weatherResult.daily.sunset[id])}
+              </div>
+            </div>
+            {/* Precipitation probability */}
+            <div className="flex flex-col items-center justify-between md:w-50 w-35   gap-3 bg-slate-500 rounded-2xl p-5  ">
+              <i className="fa-solid fa-wind text-5xl lg:text-7xl"></i>
+              <div className="md:text-2xl text-lg ">
+                Vento <br /> {weatherResult.daily.wind_speed_10m_max[id]} km/h
+              </div>
+            </div>
+            {/* Precipitation probability */}
+            <div className="flex flex-col items-center justify-between md:w-50 w-35   gap-3 bg-slate-500 rounded-2xl p-5  ">
+              <i className="fa-solid fa-water text-5xl lg:text-7xl"></i>
+              <div className="md:text-2xl text-lg ">
+                Umidità <br /> {weatherResult.hourly.relative_humidity_2m[id]} %
+              </div>
+            </div>
+            {/* Precipitation probability */}
+            <div className="flex flex-col items-center justify-between md:w-50 w-35   gap-3 bg-slate-500 rounded-2xl p-5  ">
+              <i className="fa-solid fa-temperature-arrow-up text-5xl lg:text-7xl"></i>
+              <div className="md:text-2xl text-lg ">
+                Massima <br /> {weatherResult.daily.temperature_2m_max[id]} °C
+              </div>
+            </div>
+            {/* Precipitation probability */}
+            <div className="flex flex-col items-center justify-between md:w-50 w-35   gap-3 bg-slate-500 rounded-2xl p-5  ">
+              <i className="fa-solid fa-temperature-arrow-down text-5xl lg:text-7xl"></i>
+              <div className="md:text-2xl text-lg ">
+                Minima <br /> {weatherResult.daily.temperature_2m_min[id]} °C
+              </div>
+            </div>
+          </div>
+
+          {/* <ul className=" text-xl">
             <li></li>
-          </ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li>Wind: {weatherResult.daily.wind_speed_10m_max[id]} km/h</li>
+            <li>Temp max: {weatherResult.daily.temperature_2m_max[id]} °C</li>
+            <li>Temp min: {weatherResult.daily.temperature_2m_min[id]} °C</li>
+            <li>Weather code: {weatherResult.daily.weather_code[id]}</li>
+            <li>Humidity: {weatherResult.hourly.relative_humidity_2m[id]} %</li>
+          </ul> */}
         </div>
       </div>
     </>
